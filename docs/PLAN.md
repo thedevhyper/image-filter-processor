03df378ec2116736e76f66a39ee6bcabc0a7b97e
thedevhyper <anonymous6666x@proton.me>
2025-11-05 00:20:26 +0300

    SDL and emmc setup completed, tested helloworld!.

--- docs/PLAN.md
+++ docs/PLAN.md
@@ -7,11 +7,15 @@
 
 ## Setup & Verification
 - [x] Install Emscription SDK
-- [ ] Folder Structure
-- [ ] HelloWorld.c to WASM
+- [x] Folder Structure
+- [x] HelloWorld.c to WASM
+- [ ] Run test2.c (includes SDL2) [[SDL2 MacOS Install](https://dev.to/cristiano/how-to-install-sdl2-on-macos-3pn4)]
 - [ ] Call wasm function from JS
 ## Basic Image Upload
-
+- [ ] HTML fle with input and canvas
+- [ ] Load a image in js
+- [ ] Draw image to canvas and extract image as byte array (RGBA)
+- [ ] Log pixels to verify
 ## Bridge (js <=> wasm)
 
 ## Grayscale Filter
---
+++ tests/test2.c
@@ -0,0 +1,45 @@
+// To compile: 
+// emcc tests/test2.c -o test2.html -s USE_SDL=2 -DTEST_SDL_LOCK_OPTS
+// or 
+// emcc tests/test2.c -s USE_SDL=2 -s WASM=1 -s MAX_WEBGL_VERSION=2 -s MIN_WEBGL_VERSION=2 -o test2.html  
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+int main(int argc, char** argv) {
+  printf("hello, world!\n");
+
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Surface *screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
+
+#ifdef TEST_SDL_LOCK_OPTS
+  EM_ASM("SDL.defaults.copyOnLock = false; SDL.defaults.discardOnLock = true; SDL.defaults.opaqueFrontBuffer = false;");
+#endif
+
+  if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
+  for (int i = 0; i < 256; i++) {
+    for (int j = 0; j < 256; j++) {
+#ifdef TEST_SDL_LOCK_OPTS
+      // Alpha behaves like in the browser, so write proper opaque pixels.
+      int alpha = 255;
+#else
+      // To emulate native behavior with blitting to screen, alpha component is ignored. Test that it is so by outputting
+      // data (and testing that it does get discarded)
+      int alpha = (i+j) % 255;
+#endif
+      *((Uint32*)screen->pixels + i * 256 + j) = SDL_MapRGBA(screen->format, i, j, 255-i, alpha);
+    }
+  }
+  if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
+  SDL_Flip(screen);
+
+  printf("you should see a smoothly-colored square - no sharp lines but the square borders!\n");
+  printf("and here is some text that should be HTML-friendly: amp: |&| double-quote: |\"| quote: |'| less-than, greater-than, html-like tags: |<cheez></cheez>|\nanother line.\n");
+
+  SDL_Quit();
+
+  return 0;
+}
